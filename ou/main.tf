cdcd03805d14fdae824313b6c6f1fd1cc4cf2c1f
Justin <JEStaubach@users.noreply.github.com>
2021-03-22 21:01:44 -0400

    Add snapshots back into OU.

--- ou/main.tf
+++ ou/main.tf
@@ -1,24 +1,24 @@
 module "ou" {
-  source = "github.com/xascode/modules/folder"
+  source = "github.com/xascode/modules//folder?ref=v0.1.0-alpha.6"
   
   name   = var.name
   parent = var.parent
 }
 
 module "terraform" {
-  source = "github.com/xascode/tf_modules//project?ref=v0.1.0-alpha.4"
+  source = "github.com/xascode/tf_modules//project?ref=v0.1.0-alpha.6"
   
   name   = "terraform"
   parent = {
     name = module.ou.name
     path = module.ou.path
-  }
+  }  
 
   billing_account = var.billing_account
 }
 
 module "security" {
-  source = "github.com/xascode/tf_modules//project?ref=v0.1.0-alpha.4"
+  source = "github.com/xascode/tf_modules//project?ref=v0.1.0-alpha.6"
   
   name   = "security"
   parent = {
@@ -28,9 +28,9 @@ module "security" {
 
   billing_account = var.billing_account
 }
-/*
+
 module "snapshots" {
- source = "github.com/xascode/tf_modules//project?ref=v0.1.0-alpha.4"
+ source = "github.com/xascode/tf_modules//project?ref=v0.1.0-alpha.6"
   
   name   = "snapshots"
   parent = {
@@ -40,4 +40,3 @@ module "snapshots" {
 
   billing_account = var.billing_account
 }
-*/
